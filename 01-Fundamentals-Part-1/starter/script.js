b03c2c3b17b8ea68932c61a52d0f68c5e0a664df
zibusisomasuku <45422945+zibusisomasuku@users.noreply.github.com>
2022-03-01 18:49:35 +0200

    1 March 2022

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,47 @@
+/*
+// Define Mark's parameters
+const massMark = 78;
+const heightMark = 1.69;
+
+// Define John's details
+const massJohn = 92;
+const heightJohn = 1.92;
+
+// Calculate Mark's BMI
+let markBMI = massMark / (heightMark ** 2);
+console.log(markBMI);
+
+// Calculate Mark's BMI
+let johnBMI = massJohn / (heightJohn ** 2);
+console.log(johnBMI);
+
+// Test
+markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+*/
+
+/*
+const firstName = "Zibusiso";
+const job = "analyst";
+const birthYear = 1995;
+const year = 2022;
+
+const zibusiso = "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+
+console.log(zibusiso);
+
+const zibusisoNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(zibusisoNew);
+
+console.log(`String with
+multiple
+lines`);
+*/
+
+let age = 14;
+
+if (age >= 18) {
+    console.log(`Zibusiso can start driving 🚗`);
+} else {
+    console.log(`Zibusiso can't drive yet. Wait another ${18 - age} years!`);
+}
